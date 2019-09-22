9eaa33798b83f569c68b0b7da18a11d98a6200bc
Jess Chadwick <jess.chadwick@centralreach.com>
2019-09-22 17:48:24 -0400

    Adding build folder for hosting on GitHub

---
+++ build/precache-manifest.6ab86ba61a628901636ed110b0df5a73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee60087ad61238ce016bcccde58894d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1fd20212605ed04a16a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "19cd92ca2fb6a4a90d66",
+    "url": "/static/js/2.e073ac2e.chunk.js"
+  },
+  {
+    "revision": "b1fd20212605ed04a16a",
+    "url": "/static/js/main.89f07ffc.chunk.js"
+  },
+  {
+    "revision": "f6491566ed30436e495d",
+    "url": "/static/js/runtime~main.e595960f.js"
+  }
+]);
