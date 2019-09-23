69a319803ee8ee20a1de0bb2d4ce1a7fab0d0ebe
Jess Chadwick <jess.chadwick@centralreach.com>
2019-09-23 10:26:48 -0400

    Updates

---
+++ precache-manifest.150185371f368a71ac044592853399c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ebfa6d01168df0913ac2f6dc2b763c",
+    "url": "/Splendid/index.html"
+  },
+  {
+    "revision": "d5f582dd974b98e2a312",
+    "url": "/Splendid/static/css/main.da0e5485.chunk.css"
+  },
+  {
+    "revision": "fe1d9bd55533f979d8af",
+    "url": "/Splendid/static/js/2.cccde682.chunk.js"
+  },
+  {
+    "revision": "d5f582dd974b98e2a312",
+    "url": "/Splendid/static/js/main.b1af163d.chunk.js"
+  },
+  {
+    "revision": "7cbb9cd9628501c266f0",
+    "url": "/Splendid/static/js/runtime~main.ae415d3a.js"
+  },
+  {
+    "revision": "3b7119c97c503be396b4dfd0b2c78cc8",
+    "url": "/Splendid/static/media/stock_card.3b7119c9.svg"
+  }
+]);
