33e1105efcc82681104a64b63803eabcbde76fd4
Jess Chadwick <jess.chadwick@centralreach.com>
2019-10-30 01:34:31 -0400

    Upgrading boardgame.io

--- src/Splendid/game/SplendidGame.ts
+++ src/Splendid/game/SplendidGame.ts
@@ -1,34 +1,33 @@
-import { Game } from "boardgame.io/core";
-import { GameState, GameResults } from "../../Model";
+import { GameResults } from "../../Model";
 import moves from "./moves";
 import { initializeGame as setup } from "./initializeGame";
 import { populateVisibleCards } from "../../util";
 
 export type Moves = { [key in keyof typeof moves]: (...args) => void };
 
-export const SplendidGame = Game<GameState, Moves>({
+export const SplendidGame = {
   setup,
 
   moves,
 
-  flow: {
-    movesPerTurn: 1,
-    onTurnBegin: (G, ctx) => {
+  turn: {
+    moveLimit: 1,
+    onBegin: (G, ctx) => {
       G.currentPlayerId = ctx.currentPlayer;
     },
-    onTurnEnd: (G, ctx) => {
+    onEnd: (G, ctx) => {
       // TODO: evaluate patrons
       populateVisibleCards(G.availableCards);
-    },
-    endGameIf: (G): GameResults | void => {
-      const winners = G.players.filter(x => x.prestigePoints >= 15);
+    }
+  },
+  endIf: (G): GameResults | void => {
+    const winners = G.players.filter(x => x.prestigePoints >= 15);
 
-      if (winners.length) {
-        const rankings = [...G.players].sort(
-          (a, b) => b.prestigePoints - a.prestigePoints
-        );
-        return { winner: winners[0], rankings };
-      }
+    if (winners.length) {
+      const rankings = [...G.players].sort(
+        (a, b) => b.prestigePoints - a.prestigePoints
+      );
+      return { winner: winners[0], rankings };
     }
   }
-});
+};
